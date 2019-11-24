b5cc6655b05950751839b2f561d27389e6175863
PotatoCurry <potatocurry@protonmail.com>
2019-11-24 15:00:04 -0600

    Add GUI with Gooey

--- apush.py
+++ apush.py
@@ -1,43 +1,55 @@
-import argparse
-import logging
-
 import docx
+from docx import Document
+from gooey import Gooey, GooeyParser
 from summa.summarizer import summarize
-from tqdm import tqdm
 
-import scraper
+from scraper import Scraper
 
-parser = argparse.ArgumentParser()
-parser.add_argument('outline', help='Outline Document')
-parser.add_argument('-u', '--username', help="Username", required=True)
-parser.add_argument('-p', '--password', help="Password", required=True)
-parser.add_argument('-f', '--full', help="Do not summarize the section body", action='store_true')
-parser.add_argument('-d', '--debug', help="Display debug logs", action='store_true')
-args = parser.parse_args()
 
-outline_doc = docx.Document(args.outline)
-scraper = scraper.Scraper(args.username, args.password)
-if args.debug:
-    logging.basicConfig(level=logging.DEBUG)
+@Gooey(program_name="auto-apush",
+       program_description="APUSH outline generator",
+       progress_regex=r"^(?P<current>\d+)/(?P<total>\d+)$",
+       progress_expr="current / total * 100",
+       hide_progress_msg=True)
+def main():
+    parser = GooeyParser(description="APUSH outline generator")
+    parser.add_argument("Username")
+    parser.add_argument("Password", widget='PasswordField')
+    parser.add_argument("Outline", widget='FileChooser')
+    parser.add_argument("Summarization", widget='Dropdown', choices=['Full', '75%', '50%', '25%'])
+    args = parser.parse_args()
 
-section_number = 1
-output_doc = docx.Document()
+    outline_doc: Document = docx.Document(args.Outline)
+    scraper = Scraper(args.Username, args.Password)
+    summarization_factors = {'Full': 1.0, '75%': 0.75, '50%': 0.5, '25%': 0.25}
+    summarization_factor = summarization_factors[args.Summarization]
 
-# Outline content begins at paragraph 13
-for para in tqdm(outline_doc.paragraphs[13:-1]):
-    section = para.text.strip()
-    if para.runs[0].bold:
-        logging.debug("Read heading", section)
-        output_doc.add_heading(section, 2)
-    else:
-        logging.debug("Read section", section)
-        content = scraper.section_body(section)
-        if not args.full:
-            content = summarize(content, words=100, split=True)
-        output_doc.add_paragraph(str(section_number) + ". " + section + ": " + ' '.join(content))
-        section_number += 1
+    section_number = 1
+    output_doc = docx.Document()
 
-scraper.close()
-output_filename = args.outline[:-5] + "_Complete.docx"
-output_doc.save(output_filename)
-print("Saved completed outline as", output_filename)
+    # Outline content begins at paragraph 13
+    sections = outline_doc.paragraphs[13:-1]
+    total_sections = sum(section.runs[0].bold is None for section in sections)
+
+    for paragraph in outline_doc.paragraphs[13:-1]:
+        section = paragraph.text.strip()
+        if paragraph.runs[0].bold:
+            output_doc.add_heading(section, 2)
+        else:
+            content = scraper.section_body(section)
+            if content is None:
+                print("Unable to extract body of", section)
+            else:
+                content = summarize(content, summarization_factor, split=True)
+                output_doc.add_paragraph(str(section_number) + ". " + section + ": " + ' '.join(content))
+            print("Processed", section)
+            print(str(section_number) + '/' + str(total_sections))
+            section_number += 1
+
+    scraper.close()
+    output_filename = args.Outline[:-5] + "_Complete.docx"
+    output_doc.save(output_filename)
+    print("Saved completed outline as", output_filename)
+
+
+main()
